8b2aa40f360c7fbb2f3cca71e8106ff1b2487649
xxp <xuxiaopei@ztgame.com>
2021-10-27 18:16:48 +0800

    feature:优化正态分布算法

--- include/rmi/models/normal.h
+++ include/rmi/models/normal.h
@@ -73,7 +73,12 @@ class   Normal : public  Model{
 
     L = fabs(x);
     K = 1.0 / (1.0 + 0.2316419 * L);
-    w = 1.0 - 1.0 / sqrt(2 * Pi) * exp(-L *L / 2) * (a1 * K + a2 * K *K + a3 * pow(K,3) + a4 * pow(K,4) + a5 * pow(K,5));
+    double K2=K*K;
+    double K3=K2*K;
+    double K4=K3*K;
+    double K5=K4*K;
+    w = 1.0 - 1.0 / sqrt(2 * Pi) * exp(-L *L / 2) * (a1 * K + a2 * K2 + a3 * K3 + a4 * K4+ a5 * K5);
+    //w = 1.0 - 1.0 / sqrt(2 * Pi) * exp(-L *L / 2) * (a1 * K + a2 * K *K + a3 * pow(K,3) + a4 * pow(K,4) + a5 * pow(K,5));
 
     if (x < 0 ){
       w= 1.0 - w;
